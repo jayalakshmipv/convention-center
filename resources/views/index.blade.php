c326086c53d397bc9bc7973491734303ccd14d7b
Anandskrishnan <anandsk94@gmail.com>
2016-10-06 18:27:57 +0530

    edited the content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -218,8 +218,8 @@
     <div class="row">
       <div class="col-md-12">
         <div class="section-title mb60 text-center">
-          <h1>Your Convention Centre Start Here</h1>
-          <p>Various versions have evolved over the years sometimes by accident sometimes on purpose.</p>
+          <h1 id="b">OUR VISION  </h1>
+          <p id="a">Our site is  mainly focused on to provide all the essential  details of the convention centers in our God's own country.We guide you through to make your function a memorable one &nbsp<a href="#">readmore... </a></p>
         </div>
       </div>
     </div>
@@ -228,7 +228,7 @@
         <div class="feature-block feature-center"><!-- feature block -->
           <div class="feature-icon"><img src="images/vendor.svg" alt=""></div>
           <h2><a href="find Conventioncentre.html">Find Convention centre</a></h2>
-          <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.</p>
+          <p> Here are the list of convention centers in kerala <a href="#">click on</a> </p>
         </div>
       </div>
       <!-- /.feature block -->
@@ -237,8 +237,7 @@
         <div class="feature-block feature-center"><!-- feature block -->
           <div class="feature-icon"><img src="images/mail.svg" alt=""></div>
           <h2><a href="find Contact.html">Contact</a></h2>
-          <p>Various versions have evolved over the years, sometimes by accident, sometimes on purpose 
-            (injected humour and the like).</p>
+          <p id="a">If you have any query plz free to &nbsp<a href="#">contact us</a></p>
         </div>
       </div>
       <!-- /.feature block -->
@@ -246,7 +245,7 @@
         <div class="feature-block feature-center"><!-- feature block -->
           <div class="feature-icon"><img src="images/couple.svg" alt=""></div>
           <h2><a href="find Contact.html">Special Events</a></h2>
-          <p>The generated Lorem Ipsum is therefore always free from repetition injected humour or non-characteristic words etc.</p>
+          
         </div>
       </div>
       <!-- /.feature block --> 
@@ -261,7 +260,7 @@
       <div class="col-md-12">
         <div class="section-title mb60 text-center">
           <h1>Featured Convention centre</h1>
-          <p>Many desktop publishing packages and web page editors now use orem psum as their default model text.</p>
+          <p></p>
         </div>
       </div>
     </div>
