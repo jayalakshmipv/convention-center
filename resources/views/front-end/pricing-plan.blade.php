dfae9a44e484970bf791d0b4f4fef7f60ba1a9ed
AkashKriz <akashkriz005gmail.com>
2016-10-10 15:58:40 +0530

    Price table updated

--- resources/views/front-end/pricing-plan.blade.php
+++ resources/views/front-end/pricing-plan.blade.php
@@ -41,7 +41,7 @@
               <i class="icon icon-size-60 icon-budget icon-white"></i>
           </div>
           <h1>Pricing Table</h1>
-          <p>Fusce volutpat turpis sit amet justo venenatis vestibul leo augue, molestie nec lacus utemper rhoncus arcuoin auctor sodales interdum.</p>
+          <p>We offer three main class of price range to the vendors with lot of benifits depending on the class which they choose.Gold class being the supreme one have lot of benefits, the silver class have less benefits than gold class and platinum class being the less than silver.. </p>
          
         </div>
       </div>
@@ -67,22 +67,22 @@
     <div class="row pricing-container">
       <div class="col-md-4 pricing-box pricing-box-regualr">
         <div class="well-box">
-          <h2 class="price-title">sliver</h2>
-          <h1 class="price-plan"><span class="dollor-sign">$</span>65<span class="permonth">/mo</span></h1>
-          <p>Nullam sitamet <strong>sodales magnaorem</strong> ipsumgererit ullamcorper lacus. </p>
+          <h2 class="price-title">silver</h2>
+          <h1 class="price-plan"><span class="dollor-sign"> ₹</span>1000<span class="permonth">/month</span></h1>
+          <p>As a <strong>Silver Member</strong>, you can enjoy exclusive otions of making your convention center upto mark... </p>
           <a href="#" class="btn btn-default btn-sm">Select Plan</a> </div>
         <ul class="check-circle list-group">
           <li class="list-group-item">24/7 Email Support</li>
           <li class="list-group-item">ePayments &amp; eInvoices</li>
           <li class="list-group-item">Advanced Review Management</li>
-          <li class="list-group-item">Education Webinars</li>
+          <li class="list-group-item">Management Webinars</li>
         </ul>
       </div>
       <div class="col-md-4 pricing-box pricing-box-top">
         <div class="well-box">
           <h2 class="price-title">gold</h2>
-          <h1 class="price-plan"><span class="dollor-sign">$</span>75<span class="permonth">/mo</span></h1>
-          <p>Lorem ipsum dolor <strong>sitamet consectetur</strong> adipistur exvi taequam. </p>
+          <h1 class="price-plan"><span class="dollor-sign"> ₹</span>3500<span class="permonth">/month</span></h1>
+          <p>As a <strong>Gold Member</strong>, you can enjoy exclusive otions of making your convention center upto mark... </p>
           <a href="#" class="btn btn-primary btn-sm">Select Plan</a> </div>
         <ul class="check-circle list-group">
           <li class="list-group-item">24/7 Email Support</li>
@@ -94,14 +94,14 @@
       <div class="col-md-4 pricing-box pricing-box-regualr">
         <div class="well-box">
           <h2 class="price-title">Platinum</h2>
-          <h1 class="price-plan"><span class="dollor-sign">$</span>95<span class="permonth">/mo</span></h1>
-          <p>Nullam sitamet <strong>sodales magnaorem</strong> ipsumgererit ullamcorper lacus. </p>
+          <h1 class="price-plan"><span class="dollor-sign"> ₹</span>2500<span class="permonth">/month</span></h1>
+          <p>As a <strong>Platinum Member</strong>, you can enjoy exclusive otions of making your convention center upto mark... </p>
           <a href="#" class="btn btn-default btn-sm">Select Plan</a> </div>
         <ul class="check-circle list-group">
           <li class="list-group-item">24/7 Email Support</li>
           <li class="list-group-item">ePayments &amp; eInvoices</li>
           <li class="list-group-item">Advanced Review Management</li>
-          <li class="list-group-item">Education Webinars</li>
+          <li class="list-group-item">Management Webinars</li>
         </ul>
       </div>
     </div>
@@ -115,30 +115,30 @@
       </div>
       <div class="col-md-6 question-block">
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> Which payment methods are supported?</h2>
-          <p>Sed lacinia lectus sedurna luctus interdumras commodo porttitor always faucibus Nullam sollicitudin ultriciesleo non viverra neque laoreet amazin  consectetur nisl sedblandit turpis enimac urabitur.</p>
+          <h2><span class="question-sign">1</span> Which payment methods are supported?</h2>
+          <p>Supported payment methods. SecurePay Online Payments supports payments from a number of credit cards and payment methods – including Visa and MasterCard – and it can be integrated with American Express, Diners Club International and PayPal.</p>
         </div>
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> What uisque ulrices eficiturn interdum justo?</h2>
-          <p>Sed lacinia lectus sedurna luctus interdumras commodo porttitor always faucibus Nullam sollicitudin ultriciesleo non viverra neque laoreet amazin  consectetur nisl sedblandit turpis enimac urabitur.</p>
-        </div>
+          <h2><span class="question-sign">3</span> Is there difference in Purchase Order ?</h2>
+          <p>A Purchase Order is issued by Procurement & Travel Services after a requisition is submitted to them. A Payment Request Form is used to submit an invoice for payment to Accounts Payable Services</p>
+        </div> 
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> How ullam diissim nisiege luctus anteui?</h2>
-          <p>Mauris metus leoelemetum condimentum tellus velornare dictum ligula uisque vestibulum molestieerat euleifend turpis fermentum quisras offer massaelit potentiorbi rhoncus pellen tesque exanibus. </p>
+          <h2><span class="question-sign">5</span> What is the status of my payment ?</h2>
+          <p>All departmental users can run the SC-255 report in the Enterprise Accounting System (EAS). If you require assistance in running this report, please visit EAS Training & Support and select the Learn about EAS Reports link.</p>
         </div>
       </div>
       <div class="col-md-6 question-block">
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> Can I cancel or refund my subscription?</h2>
-          <p>Phasellus quislandit velitorbi aliquet vulputate sagittis usce lobortis velsit amet facilisis imperdiet Integer lacinia sodales eratonec dignissim felisa duivulputat a triiqu nullafelis rhoncus.</p>
+          <h2><span class="question-sign">2</span> Can I cancel or refund my subscription?</h2>
+          <p>We will charge or debit your payment method at the beginning of your subscription. When we renew your Premium Membership subscription, we will use the payment method currently associated with your account. </p>
         </div>
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> Why tibulum consectetur lorem sagittis?</h2>
-          <p>Aenean varius ornare diamquis scelerisque. umsociis natoque penatibus etmagnis disarturientsitamet augue suscipit utfringilla velit sollicitudin In velest inturpis one sagittis interdu.</p>
+          <h2><span class="question-sign">4</span>  Why did I receive a check?</h2>
+          <p>If you have direct deposit with Payroll, you should receive your reimbursements direct deposited. If this is not happening, you should contact Accounts Payable.</p>
         </div>
         <div class="question-answer">
-          <h2><span class="question-sign">Q</span> Why sociis natoque magnis diaturient montes?</h2>
-          <p>Lacinia lectus sedurna luctus interdumras commodo porttitor always faucibus Nullam interdum libero utfringilla cursus tortor magna consectetur nisl sedblandit turpis urabitur.</p>
+          <h2><span class="question-sign">6</span> Can the replacement payment be issued ?</h2>
+          <p>All supplier profile modifications such as banking changes must be initiated by the supplier using the new self-service portal, iSupplyGW. Information regarding the portal, along with training documentation.</p>
         </div>
       </div>
     </div>
@@ -150,7 +150,7 @@
             <div class="feature-icon"><i class="icon-love-letter icon-size-60 icon-default"></i></div>
             <div class="feature-content">
               <h3>Have questions? Contact us at</h3>
-              <p>We're here to help, 24 hours a day, 7 days a week utfrit inturpis one sagittis interdu.<strong><a href="#"> support@weddingvendor.com</a></strong></p>
+              <p>We're here to help, 24 hours a day, 7 days a week , we are there for you.<strong><a href="#"> support@weddingvendor.com</a></strong></p>
             </div>
             <p></p>
            
@@ -162,7 +162,7 @@
             <div class="feature-icon"><i class="icon-heart-shaped-balloons icon-size-60 icon-default"></i></div>
             <div class=" feature-content">
               <h3>Want to know how it works ?</h3>
-              <p>Lacinia lectus sedurna luctus interdumras commodo pro utfringilla cursus tortor magna consectetur nisl sedblandit turpis <a href="#">Go to How it works.</a></p>
+              <p>Wedding vendor works with lot of passion and encouragement to make people easy for their wedding plans. <a href="#">Go to How it works.</a></p>
             </div>
          
         </div>
